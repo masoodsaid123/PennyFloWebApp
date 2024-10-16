f7ca98702bab97d428eb589ffb7ec0e654e88d07
masoodsaid123 <masoodsaid2001@gmail.com>
2024-10-16 21:33:28 +0530

    update og img

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -14,14 +14,14 @@ export default function App({ Component, pageProps }) {
         <title>Pennyflo - Your App Title</title>
         <meta property="og:title" content="Pennyflo - AI Cash Management Suite for Businesses" />
         <meta property="og:description" content="We understand your challenges, which is why we developed the most efficient cash flow management software on the market. It helps you automate tasks to save time, make data-driven decisions, and mitigate cash shortage risks effortlessly." />
-        <meta property="og:image" content="/img/open-graph/dashboard.jpg" />
+        <meta property="og:image" content="/assets/img/open-graph/dashboard.jpg" />
         <meta property="og:url" content="https://pennyflo.io" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Pennyflo" />
         <meta name="twitter:card" content="We understand your challenges, which is why we developed the most efficient cash flow management software on the market. It helps you automate tasks to save time, make data-driven decisions, and mitigate cash shortage risks effortlessly." />
         <meta name="twitter:title" content="Pennyflo - AI Cash Management Suite for Businesses" />
         <meta name="twitter:description" content="We understand your challenges, which is why we developed the most efficient cash flow management software on the market. It helps you automate tasks to save time, make data-driven decisions, and mitigate cash shortage risks effortlessly." />
-        <meta name="twitter:image" content="/img/open-graph/dashboard.jpg" />
+        <meta name="twitter:image" content="/assets/img/open-graph/dashboard.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
